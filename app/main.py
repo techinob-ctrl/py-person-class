d4f8b08ae5f0c6fc01f39575d01bba8398d3fcdb
Techin Rungvitayatiwat <techino.b@gmail.com>
2025-12-26 18:39:08 -0500

    Fix:

--- app/main.py
+++ app/main.py
@@ -8,30 +8,15 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    # reset class attribute ก่อนทุกครั้ง
     Person.people = {}
+    persons = [Person(data.get("name"), data.get("age")) for data in people]
 
-    # สร้างออบเจ็กต์ทั้งหมดก่อน
-    persons = [
-        Person(person_data["name"], person_data["age"]) 
-        for person_data in people
-    ]
-
-    # เชื่อมคู่สมรส
-    for person_data in people:
-        person = Person.people[person_data["name"]]
-
-        if "wife" in person_data:
-            spouse_key = "wife"
-        elif "husband" in person_data:
-            spouse_key = "husband"
-        else:
-            continue
-
-        spouse_name = person_data.get(spouse_key)
+    for data in people:
+        spouse_name = data.get("wife") or data.get("husband")
         if spouse_name is None:
             continue
-
+        spouse_key = "wife" if data.get("wife") else "husband"
+        person = Person.people[data.get("name")]
         spouse = Person.people[spouse_name]
         setattr(person, spouse_key, spouse)
 
