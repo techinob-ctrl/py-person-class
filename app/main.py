154844cda1cb20fe0765b30e9e563193d483f6e5
Techin Rungvitayatiwat <techino.b@gmail.com>
2025-12-26 18:55:04 -0500

    Solution

--- app/main.py
+++ app/main.py
@@ -16,8 +16,8 @@ def create_person_list(people: list) -> list:
         if spouse_name is None:
             continue
         spouse_key = "wife" if data.get("wife") else "husband"
-        person = Person.people[data.get("name")]
-        spouse = Person.people[spouse_name]
+        person = Person.people.get(data.get("name"))
+        spouse = Person.people.get(spouse_name)
         setattr(person, spouse_key, spouse)
 
     return persons
